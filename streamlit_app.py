81279a17503e1cdf0662144798fe63e76ca29068
Mohamed Tarek <mohamedtarek00212@gmail.com>
2026-02-16 18:47:48 +0200

    Refactor app title and add data loading
    Updated title and info message formatting, added pandas import and data loading.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
-st.title('📊 Machine Learning App')
+st.title("⚙️ Machine Learning App")
+st.info("This is app builds a machine learning model")
 
-st.info('This is app builds a machine learning model!')
+df = pd.read_excel(r"penguins_cleaned.xlsx")
+df
