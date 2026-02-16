2a839755e2f39bbb7559e01537be2dd843118acd
Mohamed Tarek <mohamedtarek00212@gmail.com>
2026-02-16 20:11:54 +0200

    Display raw data in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ st.info("This is app builds a machine learning model")
 
 with st.expander("Data"):
   url = "https://raw.githubusercontent.com/Mohamedtarek00212/dp-machinelearning/master/penguins_cleaned.xlsx"
+  st.write('**Raw data**')
   df = pd.read_excel(url)
-  st.write(df)
+  df
