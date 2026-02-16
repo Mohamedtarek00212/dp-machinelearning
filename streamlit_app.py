56f90b83bb2efb04e4431b682214d9ae92c21754
Mohamed Tarek <mohamedtarek00212@gmail.com>
2026-02-16 18:53:07 +0200

    Update data source to use GitHub link

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title("⚙️ Machine Learning App")
 st.info("This is app builds a machine learning model")
 
-df = pd.read_excel(r"penguins_cleaned.xlsx")
+df = pd.read_excel("https://github.com/Mohamedtarek00212/dp-machinelearning/blob/master/penguins_cleaned.xlsx")
 df
