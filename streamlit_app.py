3911ac782aaf61c9aca9771c9391f5bdf2aac19a
Mohamed Tarek <mohamedtarek00212@gmail.com>
2026-02-16 19:51:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,7 @@ import pandas as pd
 st.title("⚙️ Machine Learning App")
 st.info("This is app builds a machine learning model")
 
-# الرابط الخام (Raw) للملف من جيت هاب
-url = "https://raw.githubusercontent.com/Mohamedtarek00212/dp-machinelearning/master/penguins_cleaned.xlsx"
-
-# قراءة الملف مباشرة من الرابط
-df = pd.read_excel(url)
-
-# عرض الداتا
-st.write(df)
+with st.expander("Data):
+  url = "https://raw.githubusercontent.com/Mohamedtarek00212/dp-machinelearning/master/penguins_cleaned.xlsx"
+  df = pd.read_excel(url)
+  st.write(df)
