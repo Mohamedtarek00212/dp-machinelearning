8b097e2eaf98c8cd06794086d251da1033f70e77
Mohamed Tarek <mohamedtarek00212@gmail.com>
2026-02-16 19:51:49 +0200

    Fix expander title formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("⚙️ Machine Learning App")
 st.info("This is app builds a machine learning model")
 
-with st.expander("Data):
+with st.expander("Data"):
   url = "https://raw.githubusercontent.com/Mohamedtarek00212/dp-machinelearning/master/penguins_cleaned.xlsx"
   df = pd.read_excel(url)
   st.write(df)
