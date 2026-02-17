47c51a33dd1feb9f275e0cf6a5c6493360949794
Mohamed Tarek <mohamedtarek00212@gmail.com>
2026-02-17 17:09:41 +0200

    Fix indentation in data visualization section

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ with st.expander("Data"):
   y = df.species
   y
 
- with st.expander('Data visualization'):
-   # bill_length_mm	bill_depth_mm	flipper_length_mm	body_mass_g
-   st.scatter_chart(data= df, x = 'bill_length_mm', y = 'body_mass_g',color='species')
+with st.expander('Data visualization'):
+  # bill_length_mm	bill_depth_mm	flipper_length_mm	body_mass_g
+  st.scatter_chart(data= df, x = 'bill_length_mm', y = 'body_mass_g',color='species')
 
